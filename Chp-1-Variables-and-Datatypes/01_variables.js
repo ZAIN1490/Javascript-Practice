5ac3be8c173a5fd0367dd30ccfe242552deb3fca
ZAIN1490 <zainpc398077@gmail.com>
2025-07-30 13:32:19 +0500

    Chapter 1: Added variable.js

---
+++ Chp-1-Variables-and-Datatypes/01_variables.js
@@ -0,0 +1,15 @@
+// variable is a container in which we contain different data 
+// types
+
+let a = 5;
+console.log(a);
+a = "zain";           // In javascript variable can be change from one data type to other
+
+console.log(a);
+
+// Rules for assigning variables
+
+//1. letter, digits, underscore and $ sign allowed
+//2. Must begin with letter, underscore _ or $ sign
+//3. Javascript reserved words cannot be used as variable such as var let const etc
+//4.Zain and zain are different(js is case sensitive)
