b0751b20ecce7dffd0261fd807e9ecbd591e36dd
ZAIN1490 <zainpc398077@gmail.com>
2025-08-04 14:37:47 +0500

    Chapter 2: Added conditional_statements.js

---
+++ chp-2-operators/02_conditional_statements.js
@@ -0,0 +1,38 @@
+// let age = prompt("ENTER YOUR AGE");
+// if (age>18) {
+//     console.log("You can drive");
+// } else {
+//     console.log("You cannot drive");
+    
+// }
+
+
+
+// =========================switch statement=======================
+
+// const fruit = prompt("ENTER THE FRUIT");
+// switch (fruit) {
+//     case 'orange':
+//         console.log("oranges");
+//         break;
+
+//     case 'mangoes':
+//         console.log("mangoes");
+//         break;
+
+//     case 'papayas':
+//         console.log("papayas");
+//         break;
+
+//     default:
+//         console.log("sorry");
+//         break;
+//}
+
+
+
+
+
+// =============================Ternary operators==========================
+let age = prompt("ENTER YOUR AGE");
+(age>18)?console.log("You can drive"):console.log("You cannot drive");
