8e507184c4acd024ffaaeeca8e194bd051852d81
ZAIN1490 <zainpc398077@gmail.com>
2025-08-05 15:16:29 +0500

    Chapter 2:Added practice2.js

---
+++ chp-2-operators/practice2.js
@@ -0,0 +1,51 @@
+// ==============whether age lies between 10 and 20===================
+
+// let age = prompt("ENTER YOUR AGE");
+// if (age >= 10 && age <= 20) {
+//   console.log("yes");
+// } else {
+//   console.log("No");
+// }
+
+// ========================use of switch statement==========================
+
+// let num =parseInt(prompt("ENTER THE NUMBER"));
+// switch (num) {
+//   case 1:
+//     console.log("It is 1");
+//     break;
+//   case 2:
+//     console.log("It is 2");
+//     break;
+
+//   case 3:
+//     console.log("It is 3");
+//     break;
+
+//   default:
+//     console.log("It is not from option");
+//     break;
+// }
+
+
+// ==========================whether number is divisble by both 2 and 3===============
+
+// let no = prompt("ENTER THE NUMBER");
+// if (no%2==0 && no%3==0) {
+//     console.log("YES");
+// } else {
+//     console.log("No");
+    
+// }
+
+
+// ==========================whether number is divisble by either 2 or 3===============
+
+let no = prompt("Enter the number");
+if (no%2==0 || no%3==0) {
+    console.log("YES");
+} else {
+    console.log("No");
+    
+}
+
