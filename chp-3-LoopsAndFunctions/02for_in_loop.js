7fceed582b89b31d5432cedfce2deb54ef52f49a
ZAIN1490 <zainpc398077@gmail.com>
2025-08-06 09:10:07 +0500

    Chapter 3: Added 01 and 02 files

---
+++ chp-3-LoopsAndFunctions/02for_in_loop.js
@@ -0,0 +1,23 @@
+let marks = {
+  Zain: 98,
+  Ali: 87,
+  Saad: 76,
+  Noman: 89,
+};
+for (let a in marks) {
+   console.log("Marks of",a,"are",marks[a]);
+}
+
+let pointtable={
+    Pakistan:1,
+    Australlia:2,
+    India:3,
+    England:4,
+}
+for(let b in pointtable){
+    console.log(`The position of ${b} is ${pointtable[b]}`)
+}
+
+
+
+//for in loops also worked with arrays we will discussed it later
