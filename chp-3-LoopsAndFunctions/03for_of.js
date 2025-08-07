c76f737581f9bc75c4266066dd7bab170d8edb4d
ZAIN1490 <zainpc398077@gmail.com>
2025-08-07 10:27:01 +0500

    Chapter 3: Added 03_for_of.js

---
+++ chp-3-LoopsAndFunctions/03for_of.js
@@ -0,0 +1,13 @@
+//Mainly used for strings and arrays
+let marks =[78,89,67,73,90];
+for (let a of marks) {
+    console.log(a);
+}
+
+
+
+//strings
+let author = "ZAINMASOOD";
+for (let b of author) {
+    console.log(b);
+}
